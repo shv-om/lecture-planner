41595875ead6267954cbe7cf8ebbd6c770d6ac7e
ulegend <shivamchaudhary6197@gmail.com>
2021-07-05 22:15:29 +0530

    Update

---
+++ __init__.py
@@ -0,0 +1,77 @@
+"""
+Initialize Lecture Planner
+"""
+
+from os import system, name
+
+try:
+	from lectureplanner import LecturePlanner
+except ImportError as e:
+	print("Error loading Lecture Planner module :(")
+
+menu = """
+\t\t***Lecure Planner***
+\t1. Input Data
+\t2. Start Planning
+\tq. Quit (q)
+"""
+
+def clear():
+	# for windows user
+	if name == 'nt':
+		system('cls')
+	else:
+		system('clear')
+
+
+def input_batch_room(forname, forno):
+	list1 = []
+	while True:
+		name = input(f"Enter the {forname}: ")
+		no = int(input(f"Enter the {forno}: "))
+		list1.append([name, no])
+
+		if input("Add more (y/n): ") != 'y':
+			break
+
+	return list1
+
+
+
+def inputdata():
+	clear()
+	subjects = input("Enter subject list separated by a space eg: sub1 sub2... :\n").split()
+	print("\nEnter Batch Information (Name and No. of Students")
+	batches = input_batch_room('Batch Name', 'No. of students')
+	print("\nEnter Class Rooms information (Room no. and Capactiy)")
+	rooms = input_batch_room('Room No.', 'Capacity')
+
+	#pre defined for ease of testing
+	time_period = ['9:00 - 10:00', '10:00-11:00', '11:00-12:00', '12:30-1:30', '1:30-2:30', '2:30-3:30', '3:30-4:30']  #cllg time from 9:00 to 4:30
+
+	return {'subjects': subjects, 'batches': batches, 'rooms': rooms, 'time_period': time_period}
+
+
+def startplanning(data):
+	planner = LecturePlanner(data['batches'], data['rooms'], data['subjects'], data['time_period'], 4)
+	planner.print_pop()
+
+
+while True:
+	print(menu)
+	m = input()
+
+	if m == 'q':
+		print("Exiting...")
+		break
+
+	elif int(m) == 1:
+		data = inputdata()
+
+	elif int(m) == 2:
+		print("StartPlanning...")
+		startplanning(data)
+
+	else:
+		print("Wrong choice")
+
--- lectureplanner.py
+++ lectureplanner.py
@@ -5,7 +5,7 @@ Inputs:
 Batch = []    #contains number of students from each course
 rooms = []      #room no. and size of room (mention labs as well)
 subjects = []   #subject code and subject
-time-period = end time - start time
+time-period = start time - end time
 no_of_days =  # 5 or 6
 break = 1 hr  #mention lunch break time
 """
@@ -16,127 +16,123 @@ total_days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'
 
 class LecturePlanner:
 
-  def __init__(self, batches, rooms, subjects, time_period, days_count):
-    global total_days
+    def __init__(self, batches, rooms, subjects, time_period, days_count):
+        global total_days
 
-    #self.teachers = self.init_gene(teachers)
-    self.batches = self.init_gene(batches)
-    self.rooms = self.init_gene(rooms)
-    self.subjects = self.init_gene(subjects)
-    self.time_period = self.init_gene(time_period)
-    self.days = self.init_gene(total_days[:days_count])
-    self.fitness = {}
+        #self.teachers = self.init_gene(teachers)
+        self.batches = self.init_gene(batches)
+        self.rooms = self.init_gene(rooms)
+        self.subjects = self.init_gene(subjects)
+        self.time_period = self.init_gene(time_period)
+        self.days = self.init_gene(total_days[:days_count])
+        self.fitness = {}
 
-    # self.timeday = {timeday_key1: {rooms:[], subjects:[]}, timeday_key2: {...}, ... }
-    self.timeday = {}
-
-    """self.batches = batches
-    self.rooms = rooms
-    self.subjects = subjects
-    self.time_period = time_period"""
-  
-
-  def __splitter(self, chromosome):
-    return [(chromosome[i:i+4]) for i in range(0, len(chromosome), 4)]
-  
-  def __timedaydict(self, timeday_key):
-    self.timeday[timeday_key] = {'rooms': [], 'subjects': [], 'batches': []}
-
-  
-  def calc_fitness(self, population):
-    # We can calculate fitness using hashing on chromosomes
-
-    subject_day = []
-
-    for chromo in population:
-      self.fitness[chromo] = 0
-
-      # [Subject, [Batch, Limit], [Room No, Vacancy], Time Period, Day]
-      splitted_chromo = self.__splitter(chromo)
-
-      timeday_key = splitted_chromo[3] + splitted_chromo[4]
-
-      self.__timedaydict(timeday_key)
-
-      # empty room condition
-      if splitted_chromo[2] not in self.timeday[timeday_key]['rooms']:
-        self.fitness[chromo] += 1
-        self.timeday[timeday_key]['rooms'].append(splitted_chromo[2])
-      
-      if self.batches[splitted_chromo[1]][1] <= self.rooms[splitted_chromo[2]][1]:
-        self.fitness[chromo] += 1
-      
-      if splitted_chromo[0] not in self.timeday[timeday_key]['subjects']:
-        self.fitness[chromo] += 1
-        self.timeday[timeday_key]['subjects'].append(splitted_chromo[0])
-      
-      if splitted_chromo[1] not in self.timeday[timeday_key]['batches']:
-        temp = splitted_chromo[0] + splitted_chromo[1] + splitted_chromo[4]
-        if temp not in subject_day:
-          self.fitness[chromo] += 1
-          self.timeday[timeday_key]['batches'].append(splitted_chromo[1])
-          subject_day.append(temp)
+        # self.timeday = {timeday_key1: {rooms:[], subjects:[]}, timeday_key2: {...}, ... }
+        self.timeday = {}
 
 
-  def init_gene(self, l):
-    d = dict()
-    for i in range(len(l)):
-      num = f'{i:04b}'   # {0:08b}.format(i)
-      d[num] = l[i]
-    return d
+    def __splitter(self, chromosome):
+        return [(chromosome[i:i+4]) for i in range(0, len(chromosome), 4)]
 
-  
-  def init_population(self):
-    """
-    <teacher/subject, batch, room, time_period, day>
-    """
+    def __timedaydict(self, timeday_key):
+        self.timeday[timeday_key] = {'rooms': [], 'subjects': [], 'batches': []}
 
-    population = list()
 
-    # Converting Keys into Lists
-    subject = list(self.subjects)
-    batch = list(self.batches)
-    room = list(self.rooms)
+    def calc_fitness(self, population):
+        # We can calculate fitness using hashing on chromosomes
 
-    for day in self.days:
-      for time in self.time_period:
+        subject_day = []
+
+        for chromo in population:
+            self.fitness[chromo] = 0
+
+            # [Subject, [Batch, Limit], [Room No, Vacancy], Time Period, Day]
+            splitted_chromo = self.__splitter(chromo)
+
+            timeday_key = splitted_chromo[3] + splitted_chromo[4]
+
+            self.__timedaydict(timeday_key)
+
+            # empty room condition
+            if splitted_chromo[2] not in self.timeday[timeday_key]['rooms']:
+                self.fitness[chromo] += 1
+                self.timeday[timeday_key]['rooms'].append(splitted_chromo[2])
+
+            if self.batches[splitted_chromo[1]][1] <= self.rooms[splitted_chromo[2]][1]:
+                self.fitness[chromo] += 1
+
+            if splitted_chromo[0] not in self.timeday[timeday_key]['subjects']:
+                self.fitness[chromo] += 1
+                self.timeday[timeday_key]['subjects'].append(splitted_chromo[0])
+
+            if splitted_chromo[1] not in self.timeday[timeday_key]['batches']:
+                temp = splitted_chromo[0] + splitted_chromo[1] + splitted_chromo[4]
+                if temp not in subject_day:
+                    self.fitness[chromo] += 1
+                    self.timeday[timeday_key]['batches'].append(splitted_chromo[1])
+                    subject_day.append(temp)
+
+
+    def init_gene(self, l):
+        d = dict()
+        for i in range(len(l)):
+            num = f'{i:04b}'   # {0:08b}.format(i)
+            d[num] = l[i]
+        return d
+
+
+    def init_population(self):
+        """
+        <teacher/subject, batch, room, time_period, day>
+        """
+
+        population = list()
+
+        # Converting Keys into Lists
+        subject = list(self.subjects)
+        batch = list(self.batches)
+        room = list(self.rooms)
+
+        for day in self.days:
+            for time in self.time_period:
+
+                #chromosome = (random.choice(self.subjects), random.choice(self.batches), random.choice(self.rooms), time, day)
+
+                chromosome = random.choice(subject) + random.choice(batch) + random.choice(room) + time + day
+                population.append(chromosome)
+
+        return population
+
+
+    def crossover(self, chrome1, chrome2):
+        #performing single point crossover
+        c1 = list(chrome1)
+        c2 = list(chrome2)
+        #selecting a random point
+        r = random.randint(0,19)
+        #print('crossover pt:',r)
         
-        #chromosome = (random.choice(self.subjects), random.choice(self.batches), random.choice(self.rooms), time, day)
-        
-        chromosome = random.choice(subject) + random.choice(batch) + random.choice(room) + time + day
-        population.append(chromosome)
-    
-    return population
-    
-  
-  def crossover(self, chrome1, chrome2):
-    #performing single point crossover
-    c1 = list(chrome1)
-    c2 = list(chrome2)
-    #selecting a random point
-    r = random.randint(0,19)
-    #print('crossover pt:',r)
-    for i in range(r, len(c1)):
-      c1[i], c2[i] = c2[i], c1[i]
-    chrome1 = ''.join(c1)
-    chrome2 = ''.join(c2)
-    return chrome1, chrome2
+        for i in range(r, len(c1)):
+            c1[i], c2[i] = c2[i], c1[i]
+            chrome1 = ''.join(c1)
+            chrome2 = ''.join(c2)
+        return chrome1, chrome2
 
 
-  def mutation(self, chrome):
-    r = random.randint(0,19)
-    #print('mutation pt',r)
-    chrome = list(chrome)
-    chrome[r] = str(1 - int(chrome[r]))
-    chrome = ''.join(chrome)
-    return chrome
-  
+    def mutation(self, chrome):
+        r = random.randint(0,19)
+        #print('mutation pt',r)
+        chrome = list(chrome)
+        chrome[r] = str(1 - int(chrome[r]))
+        chrome = ''.join(chrome)
+        return chrome
 
-  def print_pop(self):
-    population = self.init_population()
-    
-    self.calc_fitness(population)
 
-    for chromosome in population:
-      print(chromosome, self.fitness[chromosome], ' --> ' + self.subjects[chromosome[:4]], self.batches[chromosome[4:8]], self.rooms[chromosome[8:12]], self.time_period[chromosome[12:16]], self.days[chromosome[16:]], sep=", ")
- 
+    def print_pop(self):
+        population = self.init_population()
+
+        self.calc_fitness(population)
+
+        for chromosome in population:
+            print(chromosome, self.fitness[chromosome], ' --> ' + self.subjects[chromosome[:4]], self.batches[chromosome[4:8]], self.rooms[chromosome[8:12]], self.time_period[chromosome[12:16]], self.days[chromosome[16:]], sep=", ")
+
